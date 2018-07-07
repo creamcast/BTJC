8e7d9b157b2ece843f2e91e4ae3aa18de6f091d5
BORIS <githubcream.soundwave2011@recursor.net>
2018-07-07 17:52:32 -0400

    Add files via upload

---
+++ lib/compress.sh
@@ -0,0 +1,14 @@
+java -jar compiler.jar --js=array.js --js_output_file=olib/array.js
+java -jar compiler.jar --js=block.js --js_output_file=olib/block.js
+java -jar compiler.jar --js=ejecta.js --js_output_file=olib/ejecta.js
+java -jar compiler.jar --js=generic.js --js_output_file=olib/generic.js
+java -jar compiler.jar --js=math.js --js_output_file=olib/math.js
+java -jar compiler.jar --js=canvas.js --js_output_file=olib/canvas.js
+java -jar compiler.jar --js=game.js --js_output_file=olib/game.js
+java -jar compiler.jar --js=io.js --js_output_file=olib/io.js
+java -jar compiler.jar --js=network.js --js_output_file=olib/network.js
+java -jar compiler.jar --js=string.js --js_output_file=olib/string.js
+
+
+
+
